4821853cdbed77ff031ec733f0fc3b659c22fb98
TolotraSmile <tolotra.raharison@gmail.com>
2018-01-29 13:57:52 +0300

    Delete build files

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-server_1.server.start(3000, function () { return console.log('Start listening at http://localhost:3000/'); });
-//# sourceMappingURL=index.js.map
